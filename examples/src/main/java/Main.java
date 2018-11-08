265b5c5006b9c65512248d240b813d0a5f926dd4
Andrey Skiba <a.skiba@dlg.im>
2018-11-08 16:08:26 +0300

    fix: example fix

--- examples/src/main/java/Main.java
+++ examples/src/main/java/Main.java
@@ -12,8 +12,22 @@ public class Main {
         Bot bot = Bot.start("cbb4994cabfa8d2a5bce0b5f7a44c23da943f767").get();
 
         bot.messaging().onMessage(message ->
-                System.out.println("Got a message: ")
-        );
+                bot.users().get(message.getSender()).thenAccept(userOpt -> userOpt.ifPresent(user -> {
+                            System.out.println("Got a message: " + message.getText() + " from user: " + user.getName());
+                        })
+                ).thenCompose(aVoid -> {
+                            if (message.getText().equals("Send me photo")) {
+                                bot.messaging().sendMedia(message.getPeer(), ((TextContent) message.getMessageContent()).getMedias());
+                            } else if (message.getText().equals("Send me video")) {
+                                bot.messaging().sendDocument(message.getPeer(), (DocumentContent) message.getMessageContent());
+                            } else {
+//                                bot.interactiveApi().send(message.getPeer())
+//                                bot.interactiveApi().send(message.getPeer(), new InteractiveGroup())
+                            }
+                            return null;
+                        }
+                ).thenAccept(uuid ->
+                        System.out.println("Sent a message with UUID: " + uuid)));
 
         bot.await();
     }
