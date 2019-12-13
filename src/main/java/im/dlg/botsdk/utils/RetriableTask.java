abec37ce76f8f49615259f499e8c10b441c64fbd
Dmitriy Borodkin <d.borodkin@dlg.im>
2019-12-13 11:54:37 +0300

    add scheduler

--- src/main/java/im/dlg/botsdk/utils/RetriableTask.java
+++ src/main/java/im/dlg/botsdk/utils/RetriableTask.java
@@ -1,10 +1,10 @@
 package im.dlg.botsdk.utils;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-import java.util.Date;
 import java.util.concurrent.*;
 import java.util.function.Function;
 
@@ -14,15 +14,21 @@ public class RetriableTask<R> {
     private static int MIN_DELAY;
     private static int MAX_DELAY;
     private static double DELAY_FACTOR;
-    private int retries = 0;
+    private static final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(
+                    1,
+                    new ThreadFactoryBuilder()
+                            .setDaemon(true)
+                            .setNameFormat("failAfter-%d")
+                            .build());
     private CompletableFuture<R> future = new CompletableFuture<R>();
 
     public RetriableTask(@Nullable RetryOptions retryOptions) {
         if (retryOptions == null) {
-            MAX_RETRIES = 0;
-            MIN_DELAY = 0;
-            MAX_DELAY = 0;
-            DELAY_FACTOR = 0;
+            MAX_RETRIES = 3;
+            MIN_DELAY = 1;
+            MAX_DELAY = 50;
+            DELAY_FACTOR = Math.exp(1);
         } else {
             MAX_RETRIES = retryOptions.getMaxRetries();
             MIN_DELAY = retryOptions.getMinDelay();
@@ -31,31 +37,25 @@ public class RetriableTask<R> {
         }
     }
 
-
-    public CompletableFuture<R> executeAsync(CompletableFuture<R> taskFuture) {
-        taskFuture.applyToEitherAsync(new CompletableFuture<>(), Function.identity())
-                .whenComplete((result, exception) -> {
-                    if (exception == null) {
-                        future.complete(result);
-                    } else {
+    public CompletableFuture<R> executeAsync(CompletableFuture<R> taskFuture, final int retries) {
+        long duration = 0;
+        if (retries > 0) {
+            duration = Math.min(Math.round(MIN_DELAY * Math.pow(DELAY_FACTOR, retries - 1)), MAX_DELAY);
+        }
+        scheduler.schedule(() -> {
+            taskFuture.applyToEither(new CompletableFuture<>(), Function.identity())
+                    .thenAccept(response -> future.complete(response))
+                    .exceptionally(error -> {
                         if (retries > MAX_RETRIES) {
-                            future.completeExceptionally(exception);
-                            log.error("Failed max retries request to server: ", exception);
+                            future.completeExceptionally(error);
+                            log.error("Failed max retries request to server: ", error);
                         } else {
-                            try {
-                                TimeUnit.SECONDS.sleep(Math.min(
-                                        Math.round(MIN_DELAY * Math.pow(DELAY_FACTOR, retries)),
-                                        MAX_DELAY)
-                                );
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            retries++;
-                            if (retries <= MAX_RETRIES) log.error("Failed request to server: ", exception);
-                            executeAsync(taskFuture);
+                            if (retries < MAX_RETRIES) log.error("Failed request to server: ", error);
+                            executeAsync(taskFuture, retries + 1);
                         }
-                    }
-                });
+                        return null;
+                    });
+        }, duration, TimeUnit.SECONDS);
 
         return future;
     }
